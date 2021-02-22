199a4a4f20800e5482cb68a665cd1abcda79a6b6
Nick <bykov.nv@phystech.edu>
2021-02-22 18:43:27 +0300

    update parser and adding movie's description

--- kinopoisk_parser.py
+++ kinopoisk_parser.py
@@ -4,49 +4,54 @@ import time
 import pickle
 
 
-def write_to_file(data):
-    with open('data.pickle', 'wb') as f:
+def write_pickle_to_file(data, name):
+    with open(f'data/{name}.pickle', 'wb') as f:
         pickle.dump(data, f)
 
 
-def get_top_250_films():
-    all_films_list = []
-    all_imgs_list = []
+def read_pickle_file(file_name):
+    with open(file_name, 'rb') as f:
+        data = pickle.load(f)
 
+    return data
+
+
+def get_top_250_films():
     wd = webdriver.Chrome()
+    descr_dic = {}
 
     for i in range(1, 6):
+
         wd.get(f'https://www.kinopoisk.ru/lists/top250/?page={i}&tab=all')
-        films = wd.find_elements_by_class_name('selection-film-item-meta__name')
+        film_hrefs = wd.find_elements_by_class_name('selection-film-item-meta__link')
 
-        for el in films:
-            all_films_list.append(el.text)
+        for i in range(len(film_hrefs)):
+            film_hrefs[i] = film_hrefs[i].get_attribute('href')
 
-        imgs = wd.find_elements_by_class_name('selection-film-item-poster__image')
+        for href in film_hrefs:
+            wd.get(href)
+            time.sleep(2)
 
-        for el in imgs:
-            all_imgs_list.append(el.get_attribute('src'))
-
-        time.sleep(1)
-
-    data = {}
-    for i in range(len(all_imgs_list)):
-        data[all_films_list[i]] = all_imgs_list[i]
+            film_name = wd.find_element_by_class_name('styles_title__2l0HH').text
+            text = wd.find_element_by_class_name('styles_paragraph__2Otvx').text
+            img = wd.find_element_by_class_name('film-poster').get_attribute('src')
+            descr_dic[film_name] = [img, text]
+            time.sleep(1)
 
     wd.close()
-    write_to_file(data)
+    write_pickle_to_file(descr_dic, 'main_data')
 
 
 def random_film():
-    with open('data.pickle', 'rb') as f:
-        data = pickle.load(f)
+    data = read_pickle_file('data/main_data.pickle')
 
     index = random.randint(0, len(data))
 
     film_name = list(data.keys())[index]
-    poster = data[film_name]
+    poster = data[film_name][0]
+    description = data[film_name][1]
 
-    return [film_name, poster]
+    return [film_name, poster, description]
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 from requests import get
-from myparser import random_film
+from kinopoisk_parser import random_film, read_pickle_file
 from telebot import types
 
 bot = telebot.TeleBot('')
@@ -8,30 +8,45 @@ bot = telebot.TeleBot('')
 
 @bot.message_handler(content_types=['text'])
 def start_message(message):
-    answer1 = 'Добро пожаловать! Я бот, который будет помогать вам с поиском интересных фильмов'
+    start_message = 'Добро пожаловать! Я бот, который будет помогать Вам с поиском интересных фильмов. ' \
+                    'Могу предложить Вам один из топ 250 фильмов КиноПоиска'
 
     keyboard = types.InlineKeyboardMarkup()
-    key_rfb = types.InlineKeyboardButton(text='Рандомный фильм', callback_data='random_film')
+    key_rfb = types.InlineKeyboardButton(text='Фильм из топ-250 по версии КиноПоиска', callback_data='random_film')
     keyboard.add(key_rfb)
+    '''key_rfb = types.InlineKeyboardButton(text='Выбрать жанр', callback_data='choose_genre')
+    keyboard.add(key_rfb)'''
 
-    bot.send_message(message.chat.id, answer1, reply_markup=keyboard)
+    bot.send_message(message.chat.id, start_message, reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    keyboard = types.InlineKeyboardMarkup()
-    key_rfb = types.InlineKeyboardButton(text='Да', callback_data='yes')
-    keyboard.add(key_rfb)
-    key_rfb = types.InlineKeyboardButton(text='Нет', callback_data='random_film')
-    keyboard.add(key_rfb)
 
     if call.data == 'random_film':
+        keyboard = types.InlineKeyboardMarkup()
+        key_rfb = types.InlineKeyboardButton(text='Да', callback_data='yes')
+        keyboard.add(key_rfb)
+        key_rfb = types.InlineKeyboardButton(text='Нет', callback_data='random_film')
+        keyboard.add(key_rfb)
+
         film_and_poster = random_film()
         bot.send_photo(call.from_user.id, get(film_and_poster[1]).content)
-        bot.send_message(call.from_user.id, film_and_poster[0])
+        bot.send_message(call.from_user.id, film_and_poster[0] + '\n\n' + film_and_poster[2])
         bot.send_message(call.from_user.id, 'Хотите посмотреть этот фильм?', reply_markup=keyboard)
+
+    '''    if call.data == 'choose_genre':
+        keyboard = types.InlineKeyboardMarkup()
+
+        genres = read_pickle_file('./data/genres.pickle')
+        for key, item in genres.items():
+            key_rfb = types.InlineKeyboardButton(text=key, callback_data=item)
+            keyboard.add(key_rfb)
+
+        bot.send_message(call.from_user.id, 'Хотите посмотреть этот фильм?', reply_markup=keyboard)'''
+
     if call.data == 'yes':
-        bot.send_message(call.from_user.id, 'Отлично! Приятного просмотра!')
+        bot.send_message(call.from_user.id, 'Отличный выбор! Приятного просмотра!')
 
 
 bot.polling(none_stop=True, interval=0)
